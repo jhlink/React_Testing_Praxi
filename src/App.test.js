ded1195479d96701d1d0235a762e4b1847f3a2fb
James Kim <jhlink@users.noreply.github.com>
2017-11-06 14:58:32 -0500

    feat: Working test cases fo App and Summ

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+
+it('should render label', () => {
+  const wrapper = shallow(
+      <h1>Hello Jest!</h1>
+  );
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+      <h1>Hello Jest!</h1>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
